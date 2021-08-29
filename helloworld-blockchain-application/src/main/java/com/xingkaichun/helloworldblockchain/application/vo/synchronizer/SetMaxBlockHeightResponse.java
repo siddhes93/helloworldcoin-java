009d7ec856312327b75f415790d3358a6ab025e7
sevenshi <seven_shi@qq.com>
2021-08-29 11:27:05 +0800

    设置最大挖矿高度

---
+++ helloworld-blockchain-application/src/main/java/com/xingkaichun/helloworldblockchain/application/vo/synchronizer/SetMaxBlockHeightResponse.java
@@ -0,0 +1,11 @@
+package com.xingkaichun.helloworldblockchain.application.vo.synchronizer;
+
+import com.xingkaichun.helloworldblockchain.application.vo.block.BlockVo;
+
+/**
+ *
+ * @author sevenshi seven_shi@qq.com
+ */
+public class SetMaxBlockHeightResponse {
+
+}
