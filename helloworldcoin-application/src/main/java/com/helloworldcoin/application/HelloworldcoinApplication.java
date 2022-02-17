6a2b8d3be6cbec48ffcfbafce1e8279a00e75729
x.king <xdotking@gmail.com>
2022-02-17 15:38:16 +0800

    20220217002

--- helloworldcoin-application/src/main/java/com/helloworldcoin/application/HelloworldcoinApplication.java
+++ helloworldcoin-application/src/main/java/com/helloworldcoin/application/HelloworldcoinApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 区块链应用层：包含钱包应用、区块链浏览器应用、节点控制台应用。
  *
  * @author x.king xdotking@gmail.com
  */
